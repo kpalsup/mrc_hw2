9d59ae372f19e1394995fac4b09cd81a3e1fb1ee
Katrina P. Alsup <kpalsup@nps.edu>
2018-04-12 12:12:11 -0700

    turtle

---
+++ scripts/turtleletter.sh
@@ -0,0 +1,13 @@
+
+
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, -4.6]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[4.0, 0.0, 0.0]' '[0.0, 0.0, 0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[-8.0, 0.0, 0.0]' '[0.0, 0.0, 0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[4.0, 0.0, 0.0]' '[0.0, 0.0, 0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, -1]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[4.0, 0.0, 0.0]' '[0.0, 0.0, 0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[-4.0, 0.0, 0.0]' '[0.0, 0.0, 0]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[0.0, 0.0, 0.0]' '[0.0, 0.0, -1]'
+rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[4.5, 0.0, 0.0]' '[0.0, 0.0, 0]'
+
+
